84d40247a9747ec164e5514e08c9c802e1266285
rizkybiz <justin.defrank@hashicorp.com>
2022-01-05 12:36:11 -0500

    upd: introduced the flags package for easier config and added Vault client TLS support

--- config/config.go
+++ config/config.go
@@ -3,6 +3,9 @@ package config
 import (
 	"errors"
 	"os"
+
+	vault "github.com/hashicorp/vault/api"
+	"github.com/namsral/flag"
 )
 
 type Config struct {
@@ -11,27 +14,40 @@ type Config struct {
 	DestFilePath string
 	SecretKey    string
 	SecretPath   string
+	TLS          vault.TLSConfig
 }
 
-func GetConfig(c *Config) error {
-	c.RoleID = os.Getenv("ROLE_ID")
-	c.SecretID = os.Getenv("SECRET_ID")
-	c.DestFilePath = os.Getenv("DESTINATION_FILEPATH")
-	c.SecretKey = os.Getenv("SECRET_KEY")
-	c.SecretPath = os.Getenv("SECRET_PATH")
-	if c.RoleID == "" || c.SecretID == "" {
-		return errors.New("ROLE_ID and SECRET_ID both need to be set to authenticate with Vault")
+var config Config
+
+func GetConfig() (Config, error) {
+	flag.StringVar(&config.RoleID, "role-id", "", "The configured Role ID for accessing the secret.")
+	flag.StringVar(&config.SecretID, "secret-id", "", "The Secret ID retrieved from vault.")
+	flag.StringVar(&config.DestFilePath, "destination-filepath", "", "Filepath of where to store the secret once retrieved. Should include filename.")
+	flag.StringVar(&config.SecretKey, "secret-key", "", "The name of the key where the value is stored in vault.")
+	flag.StringVar(&config.SecretPath, "secret-path", "", "The path where the KV secret engine was enabled. Generally looks like: \"{engine path name}/data/{secret name}\".")
+	vfs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "VAULT", 0)
+	vfs.StringVar(&config.TLS.CACert, "ca-cert", "", "Path to a PEM-encoded CA cert file to use to verify the vault server SSL certificate.")
+	vfs.StringVar(&config.TLS.CAPath, "ca-path", "", "Path to a directory of PEM-encoded CA cert files to verify the vault server SSL certificate.")
+	vfs.StringVar(&config.TLS.ClientCert, "client-cert", "", "Path to the certificate for Vault communication.")
+	vfs.StringVar(&config.TLS.ClientKey, "client-key", "", "Path to the private key for vault communication.")
+	vfs.StringVar(&config.TLS.TLSServerName, "tls-server-name", "", "If set, this is used to set the SNI host when connecting via TLS.")
+	vfs.BoolVar(&config.TLS.Insecure, "insecure", true, "Enables or disables SSL verification.")
+	vfs.Parse(os.Args[1:])
+	flag.Parse()
+
+	if config.RoleID == "" || config.SecretID == "" {
+		return config, errors.New("ROLE_ID and SECRET_ID both need to be set to authenticate with Vault")
 	}
-	if c.DestFilePath == "" {
-		return errors.New("DESTINATION_FILEPATH needs to be set in order to write the secret to the filesystem")
+	if config.DestFilePath == "" {
+		return config, errors.New("DESTINATION_FILEPATH needs to be set in order to write the secret to the filesystem")
 	}
-	if c.SecretKey == "" {
-		return errors.New("SECRET_KEY needs to be set to identify the Key:Value secret to be retrieved")
+	if config.SecretKey == "" {
+		return config, errors.New("SECRET_KEY needs to be set to identify the Key:Value secret to be retrieved")
 	}
-	if c.SecretPath == "" {
-		return errors.New("SECRET_PATH needs to be set in order to request the secret")
+	if config.SecretPath == "" {
+		return config, errors.New("SECRET_PATH needs to be set in order to request the secret")
 	}
-	return nil
+	return config, nil
 }
 
 func getTLSConfig(c *Config) error {
